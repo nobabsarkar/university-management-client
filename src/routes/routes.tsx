b8cea6d82c396ddb78756a1e575e601e06d2142d
Nobab Sarkar <108366419+nobabsarkar@users.noreply.github.com>
2026-01-21 21:30:55 +0600

    handle route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,7 +4,8 @@ import App from "../App";
 import Login from "../pages/Login";
 
 import Register from "../pages/Register";
-import { adminRoutes } from "./admin.routes";
+import { adminPaths } from "./admin.routes";
+import { routeGenerator } from "../utils/routesGenerator";
 
 const router = createBrowserRouter([
   {
@@ -14,17 +15,17 @@ const router = createBrowserRouter([
   {
     path: "/admin",
     element: <App />,
-    children: adminRoutes,
+    children: routeGenerator(adminPaths),
   },
   {
     path: "/faculty",
     element: <App />,
-    children: adminRoutes,
+    children: routeGenerator(adminPaths),
   },
   {
     path: "/student",
     element: <App />,
-    children: adminRoutes,
+    children: routeGenerator(adminPaths),
   },
   {
     path: "/login",
