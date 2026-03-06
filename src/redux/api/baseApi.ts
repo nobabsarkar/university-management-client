d59eb68dfd24ae7cc29d56880517de3b1ae90110
Nobab Sarkar <108366419+nobabsarkar@users.noreply.github.com>
2026-03-06 11:42:02 +0600

    use refresh token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,11 +1,59 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+import { logOut, setUser } from "../features/auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include",
+
+  // sending authorization token every request backend
+  // backend a auth guard deya asa tai joto bar request korbo toto bar token ta patate hoba
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+
+    return headers;
+  },
+});
+
+const BaseQueryWithRefreshToken = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  console.log(result);
+  console.log("Sending refresh token");
+
+  if (result?.error?.status === 401) {
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState)?.auth?.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: data?.data?.accessToken,
+        }),
+      );
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logOut());
+    }
+  }
+  return result;
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
-    credentials: "include",
-  }),
+  baseQuery: BaseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
