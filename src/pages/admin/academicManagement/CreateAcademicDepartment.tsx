87e383e01e5d0627786091b3b7e739443c6a6e1e
Nobab Sarkar <108366419+nobabsarkar@users.noreply.github.com>
2026-03-09 23:32:44 +0600

    create file

---
+++ src/pages/admin/academicManagement/CreateAcademicDepartment.tsx
@@ -0,0 +1,9 @@
+const CreateAcademicDepartment = () => {
+  return (
+    <div>
+      <h1>This is CreateAcademic Department component</h1>
+    </div>
+  );
+};
+
+export default CreateAcademicDepartment;
---
+++ src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
@@ -0,0 +1,9 @@
+const CreateAcademicFaculty = () => {
+  return (
+    <div>
+      <h1>this is Create AcademicFaculty compoent</h1>
+    </div>
+  );
+};
+
+export default CreateAcademicFaculty;
